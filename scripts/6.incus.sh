e8f180a315182600dea4f2f1a9226104dafe335f
lukexlook <luke-look@outlook.com>
2025-10-30 10:25:49 +0800

    fix: add btrfs deps and stdin preseed

--- scripts/6.incus.sh
+++ scripts/6.incus.sh
@@ -2,10 +2,10 @@
 SCRIPTS_DIR=$(dirname $(realpath $0))
 set -e
 
-sudo apt install incus qemu-system cgroup-tools
+sudo apt install incus qemu-system btrfs-progs cgroup-tools
 sudo adduser $USER incus-admin
 
-sudo incus admin init --preseed ${SCRIPTS_DIR}/../assets/incus.yaml
+cat ${SCRIPTS_DIR}/../assets/incus.yaml | sudo incus admin init --preseed
 sudo incus profile set default raw.lxc "lxc.cgroup.relative=1"
 
 MemMax=$(awk '/MemTotal/ {printf "%.0f\n", $2*0.9/1024/1024}' /proc/meminfo)
