6e7eedfa07738270598c8a5b56f981728ef7cd94
lukexlook <luke-look@outlook.com>
2025-11-14 17:16:58 +0800

    suggest using cron

--- models/nv-robot/extra.nvidia-pl.sh
+++ models/nv-robot/extra.nvidia-pl.sh
@@ -3,3 +3,11 @@ GPU_ID=${GPU_ID:-0}
 
 sudo nvidia-smi -i ${GPU_ID} -pm ENABLED
 sudo nvidia-smi -i ${GPU_ID} -pl $1
+
+echo "===
+Use this script with cron to enable power limit at boot:
+
+sudo -i
+crontab -e
+@reboot GPU_ID=${GPU_ID} $(pwd)/extra.nvidia-pl.sh $1
+"
